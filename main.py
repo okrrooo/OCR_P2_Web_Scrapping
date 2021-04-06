7c7b1cd26d2aaa893c79809994513ca348993123
Remi Krommenaker <remi.krommenaker@gmail.com>
2021-04-06 16:06:13 +0200

    Mise a jour du fichier main.py, ajout du code pour extraire les infos en fichier CSV

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
+filename = "information.csv"
+csv_writer = csv.writer(open(filename, 'w', encoding="utf-8"))
+
 
 def scrapping_book_information(url):
+
+    # where all the data will be listed
+    headers = []
+    info = []
+
+
+
 
     # Make a Get request to fetch the raw HTML content
     html_content = requests.get(url).text
@@ -16,69 +26,56 @@ def scrapping_book_information(url):
     for tr in soup.find_all("tr"):
         th = tr.find('th')
         td = tr.find('td')
-        print(th.text, td.text)
+        headers.append(th.text)
+        info.append(td.text)
 
-    # print to extract book title
+
+    # extract book title
     title = soup.find("h1")
-    print(title.text)
+    info.append(title.text)
+    headers.append('Title')
 
     # find the production description tag
     tag_product_description = soup.find('div', id='product_description')
-    print(tag_product_description.text)
+    headers.append(tag_product_description.text)
 
     # find paragraph description
     product_description = soup.find('div', id='product_description').findNextSibling().text
-    print(product_description)
+    info.append(product_description)
 
     # find product category
     category = soup.find('ul', class_='breadcrumb')
     category = category.find_all('li')[2]
     category = category.find('a', href=True)
-
-    print(category.text)
+    headers.append('Product Category')
+    info.append(category.text)
 
     # print for the book's url
 
-    print(url)
+    # find the url of the product
+    headers.append('url')
+    info.append(url)
 
     # find the review rating
     review_rating = soup.find(class_='star-rating')
-
-    print(review_rating['class'][1])
+    rating = (review_rating['class'][1])
+    headers.append('Review rating out of 5')
+    info.append(rating)
 
     # find image url
 
     image_url = soup.findAll('img', limit=1)
     for i in image_url:
 
-        print('https://books.toscrape.com/' + i['src'])
+        result = ('https://books.toscrape.com/' + i['src'])
+        headers.append('image_url')
+        info.append(result)
 
 
 
-
-
-
-
-
-
-
-
+    csv_writer.writerow(headers)
+    csv_writer.writerow(info)
 
 
 scrapping_book_information('https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html')
 
-
-
-
-
-# info a extraire :
-# product_page_url                          X
-# universal_ product_code (upc)             X
-# title                                     X
-# price_including_tax                       X
-# price_excluding_tax                       X
-# number_available                          X
-# product_description                       X
-# category                                  X
-# review_rating
-# image_url                                 x
